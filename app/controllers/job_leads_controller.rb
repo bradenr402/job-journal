47b8cf995077d15a3245dfdebd67fef610cd75da
Braden Roth <bradenr402@gmail.com>
2025-06-26 10:35:23 -0400

    remove `jbuilder` gem

--- app/controllers/job_leads_controller.rb
+++ app/controllers/job_leads_controller.rb
@@ -1,12 +1,12 @@
 class JobLeadsController < ApplicationController
   before_action :set_job_lead, only: %i[ show edit update destroy ]
 
-  # GET /job_leads or /job_leads.json
+  # GET /job_leads
   def index
     @job_leads = JobLead.all
   end
 
-  # GET /job_leads/1 or /job_leads/1.json
+  # GET /job_leads/1
   def show
   end
 
@@ -19,52 +19,42 @@ class JobLeadsController < ApplicationController
   def edit
   end
 
-  # POST /job_leads or /job_leads.json
+  # POST /job_leads
   def create
     @job_lead = Current.user.job_leads.build(job_lead_params)
 
-    respond_to do |format|
-      if @job_lead.save
-        format.html { redirect_to @job_lead, notice: 'Job lead was successfully created.' }
-        format.json { render :show, status: :created, location: @job_lead }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @job_lead.errors, status: :unprocessable_entity }
-      end
+    if @job_lead.save
+      redirect_to @job_lead, notice: 'Job lead was successfully created.'
+    else
+      render :new, status: :unprocessable_entity, alert: 'Failed to create job lead.'
     end
   end
 
-  # PATCH/PUT /job_leads/1 or /job_leads/1.json
+  # PATCH/PUT /job_leads/1
   def update
-    respond_to do |format|
-      if @job_lead.update(job_lead_params)
-        format.html { redirect_to @job_lead, notice: 'Job lead was successfully updated.' }
-        format.json { render :show, status: :ok, location: @job_lead }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @job_lead.errors, status: :unprocessable_entity }
-      end
+    if @job_lead.update(job_lead_params)
+      redirect_to @job_lead, notice: 'Job lead was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity, alert: 'Failed to update job lead.'
     end
   end
 
-  # DELETE /job_leads/1 or /job_leads/1.json
+  # DELETE /job_leads/1
   def destroy
-    @job_lead.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to job_leads_path, status: :see_other, notice: 'Job lead was successfully destroyed.' }
-      format.json { head :no_content }
+    if @job_lead.destroy
+      redirect_to job_leads_path, status: :see_other, notice: 'Job lead was successfully destroyed.'
+    else
+      redirect_to @job_lead, alert: 'Failed to destroy job lead.'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_job_lead
-      @job_lead = JobLead.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def job_lead_params
-      params.expect(job_lead: [ :title, :company, :application_url, :source, :salary, :contact, :offer_amount, :location, :status ])
-    end
+  def set_job_lead
+    @job_lead = JobLead.find(params.expect(:id))
+  end
+
+  def job_lead_params
+    params.expect(job_lead: [ :title, :company, :application_url, :source, :salary, :contact, :offer_amount, :location, :status ])
+  end
 end
