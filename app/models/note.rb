025d8a2117e4f4a9b9678b075f5df88b297437cb
Braden Roth <bradenr402@gmail.com>
2025-07-01 13:07:53 -0400

    touch parent `notable` when note is updated or created

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,15 @@
 class Note < ApplicationRecord
+  # Associations
   belongs_to :user
   belongs_to :notable, polymorphic: true
+
+  # Callbacks
+  after_save :touch_notable
+
+  # Instance Methods
+  private
+
+  def touch_notable
+    notable.touch if notable
+  end
 end
