9b0061b2a4356d1e758da3e060900e2e2155f1d9
Braden Roth <bradenr402@gmail.com>
2025-07-08 22:06:49 -0400

    add filter buttons to `notes#index`

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -3,7 +3,13 @@ class NotesController < ApplicationController
 
   # GET /notes
   def index
-    @notes = Current.user.notes.order(updated_at: :desc)
+    if params[:notable_type].present?
+      @notes = Current.user.notes.where(notable_type: params[:notable_type]).includes(:notable).order(updated_at: :desc)
+    else
+      @notes = Current.user.notes.includes(:notable).order(updated_at: :desc)
+    end
+
+    @notes = @notes.reject { it.notable.is_a?(JobLead) && it.notable.archived? }
   end
 
   # GET /notes/new
