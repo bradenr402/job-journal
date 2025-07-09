99b9a39bfb0341f8130a32924853e93cecd1debd
Braden Roth <bradenr402@gmail.com>
2025-07-08 22:32:07 -0400

    add smart wrapping for urls

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     formatted = number_with_precision(number.abs, **options)
     "#{sign}#{formatted}"
   end
+
+  # Inserts zero-width spaces (ZWSP) after "/" and "-" in URLs or long strings,
+  # allowing them to wrap only at these characters for clean, readable line breaks
+  # while preventing overflow in containers. Use in views like:
+  # <%= link_to line_wrap_url(url), url %>
+  def line_wrap_url(text)
+    safe_join(
+      text.to_s.split(/([\/-])/).map { |part| part.match?(/[\/-]/) ? part + "\u200B" : part }
+    )
+  end
 end
