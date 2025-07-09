4a07f4eb99ef94a940dbbce363588c24836802f9
Braden Roth <bradenr402@gmail.com>
2025-07-08 22:35:13 -0400

    remove `touch_notable` and add `touch: true` to notable association in Note

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,20 +1,11 @@
 class Note < ApplicationRecord
   # Associations
   belongs_to :user
-  belongs_to :notable, polymorphic: true
-
-  # Callbacks
-  after_save :touch_notable
+  belongs_to :notable, polymorphic: true, touch: true
 
   # Instance Methods
   def job_lead
     notable if notable.is_a? JobLead
     notable.job_lead if notable&.job_lead
-  end
-
-  private
-
-  def touch_notable
-    notable.touch if notable
   end
 end
