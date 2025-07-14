ab0c50a0e8258cbf1209482083f9bf60dea51630
Braden Roth <bradenr402@gmail.com>
2025-07-14 00:47:29 -0400

    fix incorrect interview 'past' search filter and prevent search filters from changing order

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,15 +9,13 @@ class SearchController < ApplicationController
 
     @selected_date_range_param = params[:date_range].presence
 
-    all_date_ranges = [ 'upcoming', 'past' ]
-    @selected_date_range = @selected_date_range_param if all_date_ranges.include?(@selected_date_range_param)
-    @unselected_date_ranges = all_date_ranges - [ @selected_date_range ].compact
+    @all_date_ranges = [ 'upcoming', 'completed' ]
+    @selected_date_range = @selected_date_range_param if @all_date_ranges.include?(@selected_date_range_param)
 
     @selected_notable_type_param = params[:notable_type].presence
 
-    all_notable_types = [ 'JobLead', 'Interview' ]
-    @selected_notable_type = @selected_notable_type_param if all_notable_types.include?(@selected_notable_type_param)
-    @unselected_notable_types = all_notable_types - [ @selected_notable_type ].compact
+    @all_notable_types = [ 'JobLead', 'Interview' ]
+    @selected_notable_type = @selected_notable_type_param if @all_notable_types.include?(@selected_notable_type_param)
 
     @query = params[:q]
     @results = SearchQuery.new(
