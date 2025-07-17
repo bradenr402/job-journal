04b715a2b9ff21868a24b9232090c79c4acbb128
Braden Roth <bradenr402@gmail.com>
2025-07-17 14:29:46 -0400

    move job lead actions into dropdown menu

---
+++ app/javascript/controllers/dropdown_controller.js
@@ -0,0 +1,44 @@
+import { Controller } from '@hotwired/stimulus';
+
+// Connects to data-controller="dropdown"
+export default class extends Controller {
+  static targets = ['menu'];
+  static classes = ['showDropdown', 'hideDropdown'];
+
+  connect() {
+    this.outsideClickListener = this.handleOutsideClick.bind(this);
+  }
+
+  toggle() {
+    if (this.menuTarget.classList.contains(this.showDropdownClass)) this.close();
+    else this.open();
+  }
+
+  handleKeydown(event) {
+    if (event.key === 'Enter' || event.key === ' ') this.toggle();
+  }
+
+  open() {
+    this.menuTarget.classList.add(this.showDropdownClass);
+    this.menuTarget.classList.remove(this.hideDropdownClass);
+
+    document.addEventListener('mousedown', this.outsideClickListener);
+    document.addEventListener('keydown', this.handleEscape);
+  }
+
+  close() {
+    this.menuTarget.classList.add(this.hideDropdownClass);
+    this.menuTarget.classList.remove(this.showDropdownClass);
+
+    document.removeEventListener('mousedown', this.outsideClickListener);
+    document.removeEventListener('keydown', this.handleEscape);
+  }
+
+  handleOutsideClick(event) {
+    if (!this.element.contains(event.target)) this.close();
+  }
+
+  handleEscape(event) {
+    if (event.key === 'Escape') this.close();
+  }
+}
