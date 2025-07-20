dec131b8e7700a25396412c0b2265f174b155d8c
Braden Roth <bradenr402@gmail.com>
2025-07-20 12:23:49 -0400

    adjust password reset notice

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -14,7 +14,7 @@ class PasswordsController < ApplicationController
       PasswordsMailer.reset(user).deliver_later
     end
 
-    redirect_to new_session_path, notice: 'Password reset instructions sent (if user with that email address exists).'
+    redirect_to new_session_path, notice: 'Password reset instructions sent to your email address (if any account with that email address exists).'
   end
 
   def edit
