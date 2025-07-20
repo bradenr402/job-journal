8f487a262b927629d2960b914432a5bd97d23390
Braden Roth <bradenr402@gmail.com>
2025-07-20 12:23:14 -0400

    add dark mode support

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,3 +7,31 @@ LocalTime.start();
 document.addEventListener('turbo:morph', () => {
   LocalTime.run();
 });
+
+// Temporarily disables all transitions
+const disableTransitionsTemporarily = () => {
+  const css = document.createElement('style');
+  css.appendChild(
+    document.createTextNode(
+      `* {
+        -webkit-transition: none !important;
+        -moz-transition: none !important;
+        -o-transition: none !important;
+        -ms-transition: none !important;
+        transition: none !important;
+      }`,
+    ),
+  );
+  document.head.appendChild(css);
+
+  // Force browser to apply the style
+  const _ = window.getComputedStyle(css).opacity;
+
+  // Remove the style after the theme has changed
+  document.head.removeChild(css);
+};
+
+// Listen for system theme changes and disable transitions during the theme switch
+window
+  .matchMedia('(prefers-color-scheme: dark)')
+  .addEventListener('change', () => disableTransitionsTemporarily());
