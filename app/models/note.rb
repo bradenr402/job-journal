751185fcd55dee7ef95eaddf3091e52016f53d82
Braden Roth <bradenr402@gmail.com>
2025-07-21 12:43:58 -0400

    extract note title logic to `title` in `note.rb`

--- app/models/note.rb
+++ app/models/note.rb
@@ -14,4 +14,16 @@ class Note < ApplicationRecord
     notable if notable.is_a? JobLead
     notable.job_lead if notable&.job_lead
   end
+
+  def title
+    case notable
+    when JobLead
+      "#{notable.title} @ #{notable.company}"
+    when Interview
+      job_lead = notable.job_lead
+      "Interview - #{job_lead.title} @ #{job_lead.company}"
+    else
+      notable.model_name.human.titlecase
+    end
+  end
 end
