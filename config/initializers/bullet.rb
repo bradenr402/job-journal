4d2a25a91df2e8207c6e3f3da09b651ddc682512
Braden Roth <bradenr402@gmail.com>
2025-07-29 20:32:59 -0400

    ignore Bullet warning for unused eager loading for `:notable`

--- config/initializers/bullet.rb
+++ config/initializers/bullet.rb
@@ -12,4 +12,5 @@ if Rails.env.development? && defined?(Bullet)
   Bullet.add_safelist type: :unused_eager_loading, class_name: 'JobLead', association: :tags
   Bullet.add_safelist type: :unused_eager_loading, class_name: 'JobLead', association: :taggings
   Bullet.add_safelist type: :unused_eager_loading, class_name: 'Interview', association: :notes
+  Bullet.add_safelist type: :unused_eager_loading, class_name: 'Note', association: :notable
 end
