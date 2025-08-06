142ddaec00564f9c9824d44abe91f44b3c57984f
Braden Roth <bradenr402@gmail.com>
2025-08-05 22:52:49 -0400

    add tests for search controller

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,8 +1,13 @@
-require "test_helper"
+require 'test_helper'
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get search_index_url
+  setup do
+    @user = users(:one)
+    sign_in_as @user
+  end
+
+  test 'should get search' do
+    get search_url
     assert_response :success
   end
 end
