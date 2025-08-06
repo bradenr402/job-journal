21061412a5bcdb8cc8eb566e01c7afa31bf7115c
Braden Roth <bradenr402@gmail.com>
2025-08-05 22:52:07 -0400

    add tests for job leads controller

--- test/controllers/job_leads_controller_test.rb
+++ test/controllers/job_leads_controller_test.rb
@@ -1,46 +1,49 @@
-require "test_helper"
+require 'test_helper'
 
 class JobLeadsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = users(:one)
+    sign_in_as @user
+
     @job_lead = job_leads(:one)
   end
 
-  test "should get index" do
-    get job_leads_url
+  test 'should get index' do
+    get job_leads_url, headers: @headers
     assert_response :success
   end
 
-  test "should get new" do
-    get new_job_lead_url
+  test 'should get new' do
+    get new_job_lead_url, headers: @headers
     assert_response :success
   end
 
-  test "should create job_lead" do
-    assert_difference("JobLead.count") do
-      post job_leads_url, params: { job_lead: {} }
+  test 'should create job_lead' do
+    assert_difference('JobLead.count') do
+      post job_leads_url, params: { job_lead: { title: 'New Job', company: 'New Co', application_url: 'https://example.com' } }, headers: @headers
     end
 
     assert_redirected_to job_lead_url(JobLead.last)
   end
 
-  test "should show job_lead" do
-    get job_lead_url(@job_lead)
+  test 'should show job_lead' do
+    get job_lead_url(@job_lead), headers: @headers
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_job_lead_url(@job_lead)
+  test 'should get edit' do
+    get edit_job_lead_url(@job_lead), headers: @headers
     assert_response :success
   end
 
-  test "should update job_lead" do
-    patch job_lead_url(@job_lead), params: { job_lead: {} }
+  test 'should update job_lead' do
+    patch job_lead_url(@job_lead), params: { job_lead: { title: 'Updated Title' } }, headers: @headers
     assert_redirected_to job_lead_url(@job_lead)
   end
 
-  test "should destroy job_lead" do
-    assert_difference("JobLead.count", -1) do
-      delete job_lead_url(@job_lead)
+  test 'should destroy job_lead' do
+    assert_difference('JobLead.count', -1) do
+      delete job_lead_url(@job_lead), headers: @headers
     end
 
     assert_redirected_to job_leads_url
