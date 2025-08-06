0dae9066cb7e98446d61120d69be42adf24df43a
Braden Roth <bradenr402@gmail.com>
2025-08-05 22:52:22 -0400

    add tests for notes controller

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -1,48 +1,60 @@
-require "test_helper"
+require 'test_helper'
 
 class NotesControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @user = users(:one)
+    sign_in_as @user
+
     @note = notes(:one)
   end
 
-  test "should get index" do
+  def notable_url(notable)
+    if notable.is_a? JobLead
+      job_lead_url(notable)
+    else
+      interview_url(notable)
+    end
+  end
+
+  test 'should get index' do
     get notes_url
     assert_response :success
   end
 
-  test "should get new" do
-    get new_note_url
+  test 'should get new' do
+    get new_note_url(notable_type: @note.notable.model_name, notable_id: @note.notable.id)
     assert_response :success
   end
 
-  test "should create note" do
-    assert_difference("Note.count") do
-      post notes_url, params: { note: {} }
+  test 'should create note' do
+    assert_difference('Note.count') do
+      post notes_url, params: { note: { content: 'This is a test.', notable_id: @note.notable_id, notable_type: @note.notable_type } }
     end
 
-    assert_redirected_to note_url(Note.last)
+    assert_redirected_to notable_url(Note.last.notable)
   end
 
-  test "should show note" do
+  test 'should show note' do
     get note_url(@note)
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get edit_note_url(@note)
     assert_response :success
   end
 
-  test "should update note" do
-    patch note_url(@note), params: { note: {} }
+  test 'should update note' do
+    patch note_url(@note), params: { note: { content: 'This is a test.' } }
     assert_redirected_to note_url(@note)
   end
 
-  test "should destroy note" do
-    assert_difference("Note.count", -1) do
+  test 'should destroy note' do
+    notable = @note.notable
+    assert_difference('Note.count', -1) do
       delete note_url(@note)
     end
 
-    assert_redirected_to notes_url
+    assert_redirected_to notable_url(notable)
   end
 end
