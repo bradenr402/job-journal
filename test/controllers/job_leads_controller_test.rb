a613eca0f70fc5250e300e7a0787f8683e0acc12
Braden Roth <bradenr402@gmail.com>
2025-08-06 15:05:58 -0400

    remove unnecessary `headers` parameter in job leads controller tests

--- test/controllers/job_leads_controller_test.rb
+++ test/controllers/job_leads_controller_test.rb
@@ -9,41 +9,41 @@ class JobLeadsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get index' do
-    get job_leads_url, headers: @headers
+    get job_leads_url
     assert_response :success
   end
 
   test 'should get new' do
-    get new_job_lead_url, headers: @headers
+    get new_job_lead_url
     assert_response :success
   end
 
   test 'should create job_lead' do
     assert_difference('JobLead.count') do
-      post job_leads_url, params: { job_lead: { title: 'New Job', company: 'New Co', application_url: 'https://example.com' } }, headers: @headers
+      post job_leads_url, params: { job_lead: { title: 'New Job', company: 'New Co', application_url: 'https://example.com' } }
     end
 
     assert_redirected_to job_lead_url(JobLead.last)
   end
 
   test 'should show job_lead' do
-    get job_lead_url(@job_lead), headers: @headers
+    get job_lead_url(@job_lead)
     assert_response :success
   end
 
   test 'should get edit' do
-    get edit_job_lead_url(@job_lead), headers: @headers
+    get edit_job_lead_url(@job_lead)
     assert_response :success
   end
 
   test 'should update job_lead' do
-    patch job_lead_url(@job_lead), params: { job_lead: { title: 'Updated Title' } }, headers: @headers
+    patch job_lead_url(@job_lead), params: { job_lead: { title: 'Updated Title' } }
     assert_redirected_to job_lead_url(@job_lead)
   end
 
   test 'should destroy job_lead' do
     assert_difference('JobLead.count', -1) do
-      delete job_lead_url(@job_lead), headers: @headers
+      delete job_lead_url(@job_lead)
     end
 
     assert_redirected_to job_leads_url
