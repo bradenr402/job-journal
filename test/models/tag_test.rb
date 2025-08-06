3c152dff008ed4e9a28e2e801d81d4e5d30ed19b
Braden Roth <bradenr402@gmail.com>
2025-08-05 22:51:42 -0400

    add tests for tags

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,7 +1,31 @@
-require "test_helper"
+require 'test_helper'
 
 class TagTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @tag = tags(:remote)
+  end
+
+  test 'should be valid with valid attributes' do
+    assert @tag.valid?
+  end
+
+  test 'should require name' do
+    @tag.name = ''
+    assert_not @tag.valid?
+    assert_includes @tag.errors[:name], "can't be blank"
+  end
+
+  test 'should require user' do
+    @tag.user = nil
+    assert_not @tag.valid?
+    assert_includes @tag.errors[:user], 'must exist'
+  end
+
+  test 'should validate uniqueness of name' do
+    @user = users(:one)
+
+    new_tag = @user.tags.build(name: @tag.name)
+    assert_not new_tag.valid?
+    assert_includes new_tag.errors[:name], 'has already been taken'
+  end
 end
--- test/models/tagging_test.rb
+++ test/models/tagging_test.rb
@@ -1,7 +1,23 @@
-require "test_helper"
+require 'test_helper'
 
 class TaggingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @tagging = taggings(:one_remote)
+  end
+
+  test 'should be valid with valid attributes' do
+    assert @tagging.valid?
+  end
+
+  test 'should require tag' do
+    @tagging.tag = nil
+    assert_not @tagging.valid?
+    assert_includes @tagging.errors[:tag], 'must exist'
+  end
+
+  test 'should require job lead' do
+    @tagging.job_lead = nil
+    assert_not @tagging.valid?
+    assert_includes @tagging.errors[:job_lead], 'must exist'
+  end
 end
