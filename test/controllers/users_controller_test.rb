d9dc0d2afddeb5c518f858d769e58f4ead52c087
Braden Roth <bradenr402@gmail.com>
2025-08-05 22:52:28 -0400

    add tests for users controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,8 +1,24 @@
-require "test_helper"
+require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get account" do
-    get users_account_url
+  setup do
+    @user = users(:one)
+    sign_in_as @user
+  end
+
+  test 'should get account' do
+    get account_url
     assert_response :success
+  end
+
+  test 'should update account' do
+    patch account_update_url(@user), params: { user: { email_address: 'new@example.com', current_password: 'password' } }
+
+    assert_redirected_to edit_account_url
+  end
+
+  test 'should prevent duplicate emails' do
+    patch account_update_url(@user), params: { user: { email_address: 'two@example.com', current_password: 'password' } }
+    assert :unprocessable_entity
   end
 end
