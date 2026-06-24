f46a322535eb27ab2cbbc356f8a8f0bf1dfd76db
Braden Roth <bradenr402@gmail.com>
2026-06-23 22:59:23 -0400

    Add `local_time_short` helper

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -2,4 +2,8 @@ module DateHelper
   def local_date_short(date)
     local_date(date, "%B %e#{', %Y' unless date.year == Time.current.year}")
   end
+
+  def local_time_short(date)
+    local_time(date, "%b %e, #{'%Y' unless date.year == Time.current.year} %l:%M%P")
+  end
 end
