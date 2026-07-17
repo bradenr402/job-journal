8f94dd1ba891a924bdf0a4590c144ea27e0ccba4
Braden Roth <bradenr402@gmail.com>
2026-07-16 22:40:43 -0400

    Fill test gaps for `Tag` and `Tagging`

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class TagTest < ActiveSupport::TestCase
-  def setup
+  setup do
     @tag = tags(:remote)
+  end
+
+  teardown do
+    Tag.destroy_all
   end
 
   test "should be valid with valid attributes" do
@@ -11,21 +15,80 @@ class TagTest < ActiveSupport::TestCase
 
   test "should require name" do
     @tag.name = ""
+
     assert_not @tag.valid?
     assert_includes @tag.errors[:name], "can't be blank"
   end
 
   test "should require user" do
     @tag.user = nil
+
     assert_not @tag.valid?
     assert_includes @tag.errors[:user], "must exist"
   end
 
-  test "should validate uniqueness of name" do
-    @user = users(:one)
+  test "name should be normalized" do
+    tag = users(:one).tags.create!(name: "MiXeD")
 
-    new_tag = @user.tags.build(name: @tag.name)
+    assert_equal "mixed", tag.reload.name
+  end
+
+  test "should validate uniqueness of name for the same user case insensitively" do
+    new_tag = users(:one).tags.build(name: @tag.name.upcase)
+
     assert_not new_tag.valid?
     assert_includes new_tag.errors[:name], "has already been taken"
+  end
+
+  test "should allow the same name for different users" do
+    new_tag = users(:two).tags.build(name: @tag.name.upcase)
+
+    assert new_tag.valid?
+  end
+
+  test "unused should return tags without taggings" do
+    unused_tag = users(:one).tags.create!(name: "unused test tag")
+    used_tag = users(:one).tags.create!(name: "used test tag")
+    used_tag.taggings.create!(job_lead: job_leads(:one))
+
+    assert_includes Tag.unused, unused_tag
+    assert_not_includes Tag.unused, used_tag
+  end
+
+  test "top_by_usage should order tags by tagging count descending" do
+    top_tag = users(:one).tags.create!(name: "top usage tag")
+    lower_tag = users(:one).tags.create!(name: "lower usage tag")
+    leads = 3.times.map { |i| create_job_lead(application_url: "https://example.com/top-tag-#{i}") }
+
+    leads.each { |lead| top_tag.taggings.create!(job_lead: lead) }
+    lower_tag.taggings.create!(job_lead: leads.first)
+
+    assert_equal top_tag, Tag.top_by_usage.first
+  end
+
+  test "cleanup_unused_for_user should destroy only unused tags for the user" do
+    user_unused_tag = users(:one).tags.create!(name: "cleanup unused")
+    user_used_tag = users(:one).tags.create!(name: "cleanup used")
+    other_user_unused_tag = users(:two).tags.create!(name: "other cleanup unused")
+
+    user_used_tag.taggings.create!(job_lead: job_leads(:one))
+
+    assert_difference -> { users(:one).tags.count }, -1 do
+      Tag.cleanup_unused_for_user(users(:one))
+    end
+
+    assert_not Tag.exists?(user_unused_tag.id)
+    assert Tag.exists?(user_used_tag.id)
+    assert Tag.exists?(other_user_unused_tag.id)
+  end
+
+  private
+
+  def create_job_lead(attributes = {})
+    users(:one).job_leads.create!({
+      title: "Example",
+      company: "Example Co.",
+      application_url: "https://example.com/jobs/tag-test-#{SecureRandom.hex(8)}"
+    }.merge(attributes))
   end
 end
--- test/models/tagging_test.rb
+++ test/models/tagging_test.rb
@@ -1,8 +1,12 @@
 require "test_helper"
 
 class TaggingTest < ActiveSupport::TestCase
-  def setup
+  setup do
     @tagging = taggings(:one_remote)
+  end
+
+  teardown do
+    Tagging.destroy_all
   end
 
   test "should be valid with valid attributes" do
@@ -11,13 +15,57 @@ class TaggingTest < ActiveSupport::TestCase
 
   test "should require tag" do
     @tagging.tag = nil
+
     assert_not @tagging.valid?
     assert_includes @tagging.errors[:tag], "must exist"
   end
 
   test "should require job lead" do
     @tagging.job_lead = nil
+
     assert_not @tagging.valid?
     assert_includes @tagging.errors[:job_lead], "must exist"
+  end
+
+  test "should validate uniqueness of tag for a job lead" do
+    duplicate = Tagging.new(job_lead: @tagging.job_lead, tag: @tagging.tag)
+
+    assert_not duplicate.valid?
+    assert_includes duplicate.errors[:tag_id], "has already been taken"
+  end
+
+  test "should allow the same tag on different job leads" do
+    tagging = Tagging.new(job_lead: create_job_lead, tag: @tagging.tag)
+
+    assert tagging.valid?
+  end
+
+  test "destroying the last tagging should destroy the orphaned tag" do
+    tag = users(:one).tags.create!(name: "orphan cleanup")
+    tagging = tag.taggings.create!(job_lead: create_job_lead(application_url: "https://example.com/orphan-cleanup"))
+
+    assert_difference -> { Tag.count }, -1 do
+      tagging.destroy
+    end
+  end
+
+  test "destroying a tagging should keep tags with other taggings" do
+    tag = users(:one).tags.create!(name: "still used")
+    first_tagging = tag.taggings.create!(job_lead: create_job_lead(application_url: "https://example.com/still-used-1"))
+    tag.taggings.create!(job_lead: create_job_lead(application_url: "https://example.com/still-used-2"))
+
+    assert_no_difference -> { Tag.count } do
+      first_tagging.destroy
+    end
+  end
+
+  private
+
+  def create_job_lead(attributes = {})
+    users(:one).job_leads.create!({
+      title: "Example",
+      company: "Example Co.",
+      application_url: "https://example.com/jobs/tagging-test-#{SecureRandom.hex(8)}"
+    }.merge(attributes))
   end
 end
