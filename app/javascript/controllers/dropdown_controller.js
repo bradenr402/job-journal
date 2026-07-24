87cbf92a76e3052f7fbfaad2583f05f244cc51b3
Braden Roth <bradenr402@gmail.com>
2026-07-24 19:44:55 -0400

    Improve dropdown keyboard control
    - Navigate between dropdown options with arrow keys
    - Trap focus inside dropdown
    - Fix using `esc` to close dropdown
    - Refocus dropdown trigger when closing dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,21 +2,21 @@ import { Controller } from '@hotwired/stimulus';
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
-  static targets = ['menu'];
+  static targets = ['menu', 'trigger'];
   static classes = ['showDropdown', 'hideDropdown'];
 
   connect() {
     this.outsideClickListener = this.handleOutsideClick.bind(this);
-    if (this.menuTarget.classList.contains(this.hideDropdownClass)) this.menuTarget.inert = true;
+    this.menuKeydownListener = this.handleMenuKeydown.bind(this);
+
+    if (this.menuTarget.classList.contains(this.hideDropdownClass)) {
+      this.menuTarget.inert = true;
+    }
   }
 
   toggle() {
     if (this.menuTarget.classList.contains(this.showDropdownClass)) this.close();
     else this.open();
-  }
-
-  handleKeydown(event) {
-    if (event.key === 'Enter' || event.key === ' ') this.toggle();
   }
 
   open() {
@@ -25,7 +25,11 @@ export default class extends Controller {
     this.menuTarget.inert = false;
 
     document.addEventListener('mousedown', this.outsideClickListener);
-    document.addEventListener('keydown', this.handleEscape);
+    document.addEventListener('keydown', this.menuKeydownListener);
+
+    this.options = [...this.menuTarget.querySelectorAll('.dropdown-option')];
+    this.selectedIndex = 0;
+    this.focusOption();
   }
 
   close() {
@@ -34,14 +38,59 @@ export default class extends Controller {
     this.menuTarget.inert = true;
 
     document.removeEventListener('mousedown', this.outsideClickListener);
-    document.removeEventListener('keydown', this.handleEscape);
+    document.removeEventListener('keydown', this.menuKeydownListener);
+
+    this.selectedIndex = null;
+  }
+
+  handleKeydown(event) {
+    switch (event.key) {
+      case 'Enter':
+      case ' ':
+        event.preventDefault();
+        this.toggle();
+        break;
+    }
+  }
+
+  handleMenuKeydown(event) {
+    if (!this.menuTarget.classList.contains(this.showDropdownClass)) return;
+
+    switch (event.key) {
+      case 'Escape':
+        this.close();
+        this.triggerTarget.focus();
+        break;
+      case 'ArrowDown':
+        event.preventDefault();
+        this.moveSelection(1);
+        break;
+      case 'ArrowUp':
+        event.preventDefault();
+        this.moveSelection(-1);
+        break;
+      case 'Tab':
+      event.preventDefault();
+        if (event.shiftKey) this.moveSelection(-1);
+        else this.moveSelection(1);
+        break;
+    }
+  }
+
+  moveSelection(direction) {
+    this.selectedIndex = Math.min(
+      Math.max(this.selectedIndex + direction, 0),
+      this.options.length - 1
+    );
+
+    this.focusOption();
+  }
+
+  focusOption() {
+    this.options[this.selectedIndex]?.focus();
   }
 
   handleOutsideClick(event) {
     if (!this.element.contains(event.target)) this.close();
-  }
-
-  handleEscape(event) {
-    if (event.key === 'Escape') this.close();
   }
 }
